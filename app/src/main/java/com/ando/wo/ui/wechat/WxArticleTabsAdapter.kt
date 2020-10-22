40475467d5b99b0b922da69990ed1fa1aa70795f
javakam <jooybao@foxmail.com>
2020-10-22 16:29:19 +0800

    BindingAdapters

--- app/src/main/java/com/ando/wo/ui/wechat/WxArticleTabsAdapter.kt
+++ app/src/main/java/com/ando/wo/ui/wechat/WxArticleTabsAdapter.kt
@@ -4,7 +4,6 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
